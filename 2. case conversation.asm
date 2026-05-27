0f5db6fcf60c8e4f912b78084e0c214fe3ebe432
imshaid <kaona30044@gmail.com>
2026-05-27 14:48:51 +0600

    Lowercase to uppercase case conversion

---
+++ 2. case conversation.asm
@@ -0,0 +1,41 @@
+; Program: Lowercase to Uppercase Case Conversion
+; Course : CSE321 - Microprocessors and Microcontrollers
+; Author : ~surjo (Md. Shaid Hasan)
+
+.MODEL  SMALL
+.STACK  100H
+
+.DATA
+    MSG1  DB  'ENTER A LOWER CASE LETTER: $'
+    MSG2  DB  0DH, 0AH, 'IN UPPER CASE IT IS: '
+    CHAR  DB  ?, '$'
+
+.CODE
+MAIN  PROC
+    ; initialize DS
+    MOV  AX, @DATA
+    MOV  DS, AX
+
+    ; display prompt
+    LEA  DX, MSG1
+    MOV  AH, 9
+    INT  21H
+
+    ; read lowercase letter
+    MOV  AH, 1
+    INT  21H
+
+    ; convert to uppercase
+    SUB  AL, 20H
+    MOV  CHAR, AL
+
+    ; display result
+    LEA  DX, MSG2
+    MOV  AH, 9
+    INT  21H
+
+    ; exit
+    MOV  AH, 4CH
+    INT  21H
+MAIN  ENDP
+END   MAIN
